fc20485075102f989ec7460f3d9f691ec2edab35
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-02-01 15:11:38 +0530

    Update bancodes.py

--- Lovely_System/plugins/bancodes.py
+++ Lovely_System/plugins/bancodes.py
@@ -1,26 +1,26 @@
    
 help_plus = """ Here is Help for **Bancodes** :-
-**✯** `{LXSx00}` **SCAMMER.**
-**✯** `{LXSx01}` **SPAM ADDING MEMBER.**
-**✯** `{LXSx03}` **ABUSE SPAM.**
-**✯** `{LXSx04}` **NSFW SPAMMER.**
-**✯** `{LXSx06}` **IMPERSONATION.**
-**✯** `{LXSx07}` **MD/BTC SCAM.**
-**✯** `{LXSx08}` **ADDING SPAMBOTS.**
-**✯** `{LXSx10}` **ILLEGAL.**
-**✯** `{LXSx11}` **PHISHING.**
-**✯** `{LXSx12}` **FRAUD PROMOTION  [ANY KIND].**
-**✯** `{LXSx13}` **CYBER THREATENING / CYBER BULLY.**
-**✯** `{LXSx14}` **CHILD ABUSE.**
-**✯** `{LXSx15}` **BAN EVASION.**
-**✯** `{LXSx16}` **SPAMBOT.**
-**✯** `{LXSx17}` **RAID INITIALIZOR.** 
-**✯** `{LXSx18}` **RAID PARTICIPANT.**
-**✯** `{LXSx19}` **KRIMINALANT.**
-**✯** `{LXSx20}` **SPAMMING VIOLENT CONTENT.**
-**✯** `{LXSx21}` **HATE SPEECH AGAINST NATIONALITY.**
-**✯** `{LXSx22}` **RAID/SPAM INFLAMMER.**
-**✯** `{LXSx23}` **PRONOGRAPHY CCONTENT PROMOTING.**
-**✯** `{LXSx24}` **PAID GIRL.**
+**✪** `{PGx00}` **SCAMMER.**
+**✪** `{PGx01}` **SPAM ADDING MEMBER.**
+**✪** `{PGx03}` **ABUSE SPAM.**
+**✪** `{PGx04}` **NSFW SPAMMER.**
+**✪** `{PGx06}` **IMPERSONATION.**
+**✪** `{PGx07}` **MD/BTC SCAM.**
+**✪** `{PGx08}` **ADDING SPAMBOTS.**
+**✪** `{PGx10}` **ILLEGAL.**
+**✪** `{PGx11}` **PHISHING.**
+**✪** `{PGx12}` **FRAUD PROMOTION  [ANY KIND].**
+**✪** `{PGx13}` **CYBER THREATENING / CYBER BULLY.**
+**✪** `{PGx14}` **CHILD ABUSE.**
+**✪** `{PGx15}` **BAN EVASION.**
+**✪** `{PGx16}` **SPAMBOT.**
+**✪** `{PGx17}` **RAID INITIALIZOR.** 
+**✪** `{PGx18}` **RAID PARTICIPANT.**
+**✪** `{PGx19}` **KRIMINALANT.**
+**✪** `{PGx20}` **SPAMMING VIOLENT CONTENT.**
+**✪** `{PGx21}` **HATE SPEECH AGAINST NATIONALITY.**
+**✪** `{PGx22}` **RAID/SPAM INFLAMMER.**
+**✪** `{PGx23}` **PRONOGRAPHY CCONTENT PROMOTING.**
+**✪** `{PGx24}` **PAID GIRL.**
 """
 __plugin_name__ = "bancodes"
